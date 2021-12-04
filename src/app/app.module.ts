d35c5345bdd57faccbdec475851f2d05e2b703f3
Ankit <ankankit@cisco.com>
2021-12-04 21:46:10 +0530

    angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { environment } from "src/environments/environment";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFireModule } from "@angular/fire/compat";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EmployeeEffect } from './state/effects/employee.effect';
-import { EffectsModule } from '@ngrx/effects';
-import { employeeReducer } from './state/reducers/employee.reducer';
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EmployeeEffect } from "./state/effects/employee.effect";
+import { EffectsModule } from "@ngrx/effects";
+import { employeeReducer } from "./state/reducers/employee.reducer";
 
-import { HomeComponent } from './home/home.component';
-import { EmployeeFormComponent } from './employee-form/employee-form.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { cityReducer } from './state/reducers/city.reducer';
-import { CityEffect } from './state/effects/city.effect';
+import { HomeComponent } from "./home/home.component";
+import { EmployeeFormComponent } from "./employee-form/employee-form.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { cityReducer } from "./state/reducers/city.reducer";
+import { CityEffect } from "./state/effects/city.effect";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     EmployeeFormComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,20 +33,20 @@ import { CityEffect } from './state/effects/city.effect';
     FormsModule,
     StoreModule.forRoot({
       employee: employeeReducer,
-      city: cityReducer
+      city: cityReducer,
     }),
     StoreDevtoolsModule.instrument({
-      maxAge: 25
+      maxAge: 25,
     }),
     EffectsModule.forRoot([EmployeeEffect, CityEffect]),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'add-employee', component: EmployeeFormComponent },
-      { path: 'employee/edit/:id', component: EmployeeFormComponent },
-      { path: '**', component: HomeComponent }
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "add-employee", component: EmployeeFormComponent },
+      { path: "employee/edit/:id", component: EmployeeFormComponent },
+      { path: "**", component: HomeComponent },
     ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,57 +1,64 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { Employee } from 'src/models/employee';
-import { City } from 'src/models/city';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { Employee } from "src/models/employee";
+import { City } from "src/models/city";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EmployeeService {
-
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) {}
 
   getCityList(): Observable<City[]> {
-    const cityList = this.db.collection<City>('City').snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(
-          c => ({
+    const cityList = this.db
+      .collection<City>("City")
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((c) => ({
             cityId: c.payload.doc.id,
-            ...c.payload.doc.data()
+            ...c.payload.doc.data(),
           }));
-      }));
+        })
+      );
     return cityList;
   }
 
   saveEmployee(employee: Employee) {
     const employeeData = JSON.parse(JSON.stringify(employee));
-    return this.db.collection('Employee').add(employeeData);
+    return this.db.collection("Employee").add(employeeData);
   }
 
   getAllEmployees(): Observable<Employee[]> {
-    const employeeList = this.db.collection<Employee>('Employee').snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(
-          c => ({
+    const employeeList = this.db
+      .collection<Employee>("Employee")
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((c) => ({
             employeeId: c.payload.doc.id,
-            ...c.payload.doc.data()
+            ...c.payload.doc.data(),
           }));
-      }));
+        })
+      );
     return employeeList;
   }
 
   getEmployeeById(employeeId: string): Observable<Employee> {
-    const employeeData = this.db.doc<Employee>('Employee/' + employeeId).valueChanges();
+    const employeeData = this.db
+      .doc<Employee>("Employee/" + employeeId)
+      .valueChanges();
     return employeeData;
   }
 
   updateEmployee(employeeId: string, employee: Employee) {
     const employeeData = JSON.parse(JSON.stringify(employee));
-    return this.db.doc('Employee/' + employeeId).update(employeeData);
+    return this.db.doc("Employee/" + employeeId).update(employeeData);
   }
 
   deleteEmployee(employeeId: string) {
-    return this.db.doc('Employee/' + employeeId).delete();
+    return this.db.doc("Employee/" + employeeId).delete();
   }
 }
